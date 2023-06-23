ecf782d2a1073e7d283e8d5664ccdf5984e49da7
AutumnalBlake <bmorri9999@gmail.com>
2023-06-23 13:09:54 +1000

    fri wk4

---
+++ F12A/Wk4/BSTreeCountGreater.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "BSTree.h"
+
+int BSTreeCountGreater(BSTree t, int key) {
+  return 0;
+}
+
+int main(int argc, char *argv[]) {
+  BSTree t = readBSTree(0);
+  printBSTree(t);
+  printf("Enter key to find: ");
+  int k;
+  scanf("%d", &k);
+  printf("Num greater than %d: %d\n", k, BSTreeCountGreater(t, k));
+  freeBSTree(t);
+}
