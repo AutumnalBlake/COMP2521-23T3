dd23b2e4352e4458993c2fe62d0f7ffebb7012a7
Blake Morris <bmorri9999@gmail.com>
2023-07-19 08:02:32 +1000

    wk8

--- Starter/Wk7/Queue.c
+++ Starter/Wk7/Queue.c
@@ -1,127 +1,13 @@
-// Queue.c
-
 #include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "Queue.h"
-
-struct node {
-	int          item;
-	struct node *next;
-};
-
-struct queue {
-	struct node *front;
-	struct node *back;
-	int          size;
-};
-
-static struct node *createNode(int item);
-
-// Create a new queue
-Queue QueueNew(void) {
-	Queue new = malloc(sizeof(*new));
-	if (new == NULL) {
-		fprintf(stderr, "QueueNew: Insufficient memory!\n");
-		exit(EXIT_FAILURE);
-	}
-	
-	new->front = NULL;
-	new->back = NULL;
-	new->size = 0;
-	return new;
-}
-
-// Free all resources allocated for the queue
-void QueueFree(Queue q) {
-	assert(q != NULL);
-	
-	struct node *curr = q->front;
-	while (curr != NULL) {
-		struct node *temp = curr;
-		curr = curr->next;
-		free(temp);
-	}
-	free(q);
-}
-
-// Add an item to the end of the queue
-void QueueEnqueue(Queue q, int item) {
-	struct node *new = createNode(item);
-	if (q->size == 0) {
-		q->front = new;
-	} else {
-		q->back->next = new;
-	}
-	q->back = new;
-	q->size++;
-}
-
-static struct node *createNode(int item) {
-	struct node *new = malloc(sizeof(*new));
-	if (new == NULL) {
-		fprintf(stderr, "QueueEnqueue: Insufficient memory!\n");
-		exit(EXIT_FAILURE);
-	}
-	new->item = item;
-	new->next = NULL;
-	return new;
-}
-
-// Remove an item from the front of the queue and return it
-int QueueDequeue(Queue q) {
-	assert(q != NULL);
-	assert(QueueSize(q) > 0);
-	
-	int item = q->front->item;
-	struct node *newFront = q->front->next;
-	free(q->front);
-	q->front = newFront;
-	q->size--;
-	
-	if (q->size == 0) {
-		q->back = NULL;
-	}
-	return item;
-}
-
-// Get the item at the front of the queue (without removing it)
-int QueuePeek(Queue q) {
-	assert(q != NULL);
-	assert(QueueSize(q) > 0);
-	
-	return q->front->item;
-}
-
-// Get the number of items in the queue
-int QueueSize(Queue q) {
-	assert(q != NULL);
-	
-	return q->size;
-}
-
-// Check if the queue is empty
-bool QueueIsEmpty(Queue q) {
-	assert(q != NULL);
-	
-	return (QueueSize(q) == 0);
-}
-
-// Print the queue to an open file (for debugging)
-void QueueDump(Queue q, FILE *fp) {
-	assert(q != NULL);
-
-	struct node *curr = q->front;
-	while (curr != NULL) {
-		if (curr != q->front) {
-			fprintf(fp, " ");
-		}
-		fprintf(fp, "%d", curr->item);
-		curr = curr->next;
-	}
-	fprintf(fp, "\n");
-}
-
-
+#define s struct
+#define a assert
+#define f free
+#define m malloc
+#define r return
+#define v void
+#define Q Queue
+s n {int i;s n *x;};s queue {s n *f;s n *b;int z;};static s n *cn(int i);Q QueueNew(v){Q w=m(sizeof(*w));if(w == 0){fprintf(stderr, "QueueNew: Insufficient memory!\n");exit(EXIT_FAILURE);} w->f=0;w->b=0;w->z=0;r w;}v QueueFree(Q q){a(q!=0);s n *c=q->f;while(c!=0){s n *t=c;c=c->x;f(t);} f(q);}v QueueEnqueue(Q q, int i){s n *w=cn(i);if(q->z == 0){q->f=w;}else{q->b->x=w;} q->b=w;q->z++;}static s n *cn(int i){s n *w=m(sizeof(*w));if(w == 0){fprintf(stderr, "QueueEnqueue: Insufficient memory!\n");exit(EXIT_FAILURE);} w->i=i;w->x=0;r w;}int QueueDequeue(Q q){a(q!=0);a(QueueSize(q)> 0);int i=q->f->i;s n *wf=q->f->x;f(q->f);q->f=wf;q->z--;if(q->z == 0){q->b=0;} r i;}int QueuePeek(Q q){a(q!=0);a(QueueSize(q)> 0);r q->f->i;}int QueueSize(Q q){a(q!=0);r q->z;}bool QueueIsEmpty(Q q){a(q!=0);r (QueueSize(q)== 0);}v QueueDump(Q q, FILE *fp){a(q!=0);s n *c=q->f;while(c!=0){if(c!=q->f){fprintf(fp, " ");} fprintf(fp, "%d", c->i);c=c->x;} fprintf(fp, "\n");}
